31c2c1db92c5dd1b43cb6ad3fd12cea7c0bb04ac
Amanda <a.mandallral15@gmail.com>
2025-09-26 10:33:39 -0300

    commit

---
+++ 02-tipos-de-dados.py
@@ -0,0 +1,17 @@
+nome = "Amanda" #string
+sala = 3 #int
+altura = 1.78 #float
+is_student = False #booleano
+notas = [8.5, 9.0, 7.5] #lista = array
+numero = (1, 2, 3) #tupla
+pessoa = {"nome": "Fernando", "disciplina": "Metaeuristicas"} #dicionario  que parece com struct
+conjunto = {"vermelho", "azul", "verde"} #conjunto
+
+print("Nome: "+nome+" sala: "+str(sala)+" altura: "+ str(altura)+ " estudante: "+ str(is_student))
+
+print(f"notas: {notas}")
+
+print(f"Numeros: {numero}")
+print(f"Pessoa: {pessoa}")
+print(f"Cores: {conjunto}")
+
---
+++ Python/01-operacoes-aritmeticas.py
@@ -0,0 +1,18 @@
+a = 10 #declaracao de variavel nao precisa de tipo
+b = 3
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+divisao_inteira = a // b
+resto_divisao = a % b
+exponenciacao = a ** b
+
+print(f"Soma: {soma}") #f indica formatado
+print(f"Substracao: {subtracao}")
+print(f"Multiplicacao: {multiplicacao}")
+print(f"Divisao: {divisao}") # os valores podem mudar de inteiro para float 
+print(f"Divisao Inteira: {divisao_inteira}")
+print(f"Resto da Divisao: {resto_divisao}")
+print(f"Exponenciacao: {exponenciacao}")
